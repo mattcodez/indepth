c90f2dd3710e6a6a4920fc86f99c7d2bad2f0b40
Matthew Molnar <matthew.molnar@gmail.com>
2023-05-01 22:38:49 -0400

    Updating to new tests but the app is getting run inside the immediate path and not inside the test dir

--- test.js
+++ test.js
@@ -1,36 +1,56 @@
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
 
-const { listMarkdownFiles } = require('./main.js');
+const app = require('./main.js');
 
 const directoryPath = './';
+const TOCFilename = 'indepth.md';
 
 describe('listMarkdownFiles', () => {
   beforeAll(() => {
-    exec('touch example.md example2.md example3.txt');
-    fs.mkdirSync('test-dir');
-    fs.writeFileSync('test-dir/test.md', '# Test file');
+    // Create test directory and files
+    fs.mkdirSync('testDir');
+    fs.writeFileSync('testDir/test1.md', '# Test 1');
+    fs.writeFileSync('testDir/test2.md', '# Test 2');
+    fs.writeFileSync('testDir/test3.md', 'No markdown header');
+    fs.writeFileSync('testDir/test with spaces.md', '# Test with spaces');
   });
 
   afterAll(() => {
-    exec('rm example.md example2.md example3.txt');
-    fs.unlinkSync('indepth.md');
-    fs.unlinkSync('test-dir/test.md');
-    fs.rmdirSync('test-dir');
+    // Remove test directory and files
+    fs.unlinkSync('testDir/test1.md');
+    fs.unlinkSync('testDir/test2.md');
+    fs.unlinkSync('testDir/test3.md');
+    fs.unlinkSync('testDir/test with spaces.md');
+    fs.unlinkSync(TOCFilename);
+    fs.rmdirSync('testDir');
   });
 
-  it('should create a new markdown file with links to other markdown files in the current directory', async () => {
-    await listMarkdownFiles();
+  test('generateMarkdownList() should return a list of markdown files in a directory', async () => {
+    const expected = ['- [Test 1](test1.md)', '- [Test 2](test2.md)', '- [test with spaces](test%20with%20spaces.md)'];
+    await app.listMarkdownFiles();
+    const actual = fs.readFileSync(TOCFilename, 'utf8').split('\n').map(x => x.trim());
+    expect(actual).toEqual(expected);
+  });
 
-    const currentDirectory = path.basename(process.cwd());
-    const newFilePath = path.join(directoryPath, `${currentDirectory}.md`);
+  test('generateMarkdownList() should not include files without a markdown header', async () => {
+    const expected = ['- [Test 1](test1.md)', '- [Test 2](test2.md)', '- [test with spaces](test%20with%20spaces.md)'];
+    await app.listMarkdownFiles();
+    const actual = fs.readFileSync(TOCFilename, 'utf8').split('\n').map(x => x.trim());
+    expect(actual).toEqual(expected);
+  });
 
-    const fileContents = await fs.readFileSync(newFilePath, 'utf-8');
-    expect(fileContents).toContain(`# ${currentDirectory}`);
-    expect(fileContents).toContain('- [example]');
-    expect(fileContents).toContain('- [example2]');
-    expect(fileContents).not.toContain('- [example3]');
-    expect(fileContents).toContain('- [Test file]');
+  test('generateMarkdownList() should use the first markdown header as the link title', async () => {
+    const expected = ['- [Test 1](test1.md)', '- [Test 2](test2.md)', '- [Test with spaces](test%20with%20spaces.md)'];
+    await app.listMarkdownFiles();
+    const actual = fs.readFileSync(TOCFilename, 'utf8').split('\n').map(x => x.trim());
+    expect(actual).toEqual(expected);
+  });
+
+  test('generateMarkdownList() should use the file name if there is no markdown header', async () => {
+    const expected = ['- [Test 1](test1.md)', '- [Test 2](test2.md)', '- [test3.md](test3.md)', '- [test with spaces](test%20with%20spaces.md)'];
+    await app.listMarkdownFiles();
+    const actual = fs.readFileSync(TOCFilename, 'utf8').split('\n').map(x => x.trim());
+    expect(actual).toEqual(expected);
   });
 });
