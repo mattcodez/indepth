536b659ae493b2804418fc3e00c90d42df5a8aa7
Matthew Molnar <matthew.molnar@gmail.com>
2023-05-01 22:23:59 -0400

    Fixing chatgpt tests

--- main.js
+++ main.js
@@ -39,3 +39,6 @@ async function listMarkdownFiles() {
 }
 
 listMarkdownFiles();
+module.exports = {
+    listMarkdownFiles
+  };
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
+
+const { listMarkdownFiles } = require('./main.js');
 
 const directoryPath = './';
 
@@ -13,9 +15,9 @@ describe('listMarkdownFiles', () => {
 
   afterAll(() => {
     exec('rm example.md example2.md example3.txt');
-    fs.rmdirSync('test-dir');
-    fs.unlinkSync('test.md');
+    fs.unlinkSync('indepth.md');
     fs.unlinkSync('test-dir/test.md');
+    fs.rmdirSync('test-dir');
   });
 
   it('should create a new markdown file with links to other markdown files in the current directory', async () => {
@@ -24,7 +26,7 @@ describe('listMarkdownFiles', () => {
     const currentDirectory = path.basename(process.cwd());
     const newFilePath = path.join(directoryPath, `${currentDirectory}.md`);
 
-    const fileContents = await fs.readFile(newFilePath, 'utf-8');
+    const fileContents = await fs.readFileSync(newFilePath, 'utf-8');
     expect(fileContents).toContain(`# ${currentDirectory}`);
     expect(fileContents).toContain('- [example]');
     expect(fileContents).toContain('- [example2]');
